e5c9f516bf5a6b10f2ff98b8500dad035691c5a0
choidongkuen <danaver12@daum.net>
2024-02-23 23:29:31 +0900

    :sparkles: feat: 게시글 상세조회,목록조회 시 태그 필드 추가

--- src/main/kotlin/com/todo/api/dto/board/GetBoardsRequestBySearch.kt
+++ src/main/kotlin/com/todo/api/dto/board/GetBoardsRequestBySearch.kt
@@ -3,14 +3,16 @@ package com.todo.api.dto.board
 import com.todo.service.board.dto.GetBoardsRequestDto
 
 // Board 목록 조회 요청 DTO
-data class GetBoardsRequest(
+// 동적 쿼리 : title,createdBy,firstTag
+data class GetBoardsRequestBySearch(
     val title: String? = null,
     val createdBy: String? = null,
-    val tag: String? = null,
+    val firstTag: String? = null,
 )
 
-fun GetBoardsRequest.toDto() =
+fun GetBoardsRequestBySearch.toDto() =
     GetBoardsRequestDto(
         title = title,
         createdBy = createdBy,
+        firstTag = firstTag,
     )
