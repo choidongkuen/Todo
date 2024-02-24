40db3b145db5b418ebd74d6f4c1172aa2ddadf66
choidongkuen <danaver12@daum.net>
2024-02-25 06:34:02 +0900

    :recycle: refactor: Like 엔티티 리팩토링

--- src/main/kotlin/com/todo/domain/entity/Like.kt
+++ src/main/kotlin/com/todo/domain/entity/Like.kt
@@ -1,6 +1,7 @@
 package com.todo.domain.entity
 
 import com.todo.core.entity.BaseTimeEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -13,8 +14,13 @@ class Like(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+    createdBy: String,
     board: Board,
 ) : BaseTimeEntity() {
+    @Column(name = "created_by", nullable = false)
+    var createdBy: String = createdBy
+        protected set
+
     @ManyToOne
     @JoinColumn(name = "booard_id")
     var board: Board = board
--- src/main/kotlin/com/todo/domain/repository/LikeRepository.kt
+++ src/main/kotlin/com/todo/domain/repository/LikeRepository.kt
@@ -3,5 +3,4 @@ package com.todo.domain.repository
 import com.todo.domain.entity.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository: JpaRepository<Like,Long> {
-}
+interface LikeRepository : JpaRepository<Like, Long>
